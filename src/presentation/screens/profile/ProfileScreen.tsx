63b0745cfa03dea535b184250216ccad6b4db25b
ArielC98 <arielcalderon100@gmail.com>
2025-06-03 12:50:17 -0500

    Practice finished

--- src/presentation/screens/profile/ProfileScreen.tsx
+++ src/presentation/screens/profile/ProfileScreen.tsx
@@ -1,4 +1,4 @@
-import { Text, View } from "react-native"
+import { Pressable, Text, View } from "react-native"
 import { styles } from "../../../config"
 import { useProfileStore } from "../../store/profile-store"
 
@@ -7,11 +7,33 @@ export const ProfileScreen = () => {
 
   const name = useProfileStore(state => state.name);
   const email = useProfileStore(state => state.email);
+  const changeProfile = useProfileStore(state => state.changeProfile);
+
 
   return (
-    <View style= {styles.container}>
-      <Text style = {styles.title}>{name}</Text>
-      <Text style = {styles.title}>{email}</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>{name}</Text>
+      <Text style={styles.title}>{email}</Text>
+
+      <Pressable
+        style={styles.primaryButton}
+        onPress={() => useProfileStore.setState({name:'Ariel Calderon'})}
+      >
+        <Text>Cambiar nombre</Text>
+      </Pressable>
+      <Pressable
+        style={styles.primaryButton}
+        onPress={() => useProfileStore.setState({email:'arielcalderon100@gmail.com'})}
+      >
+        <Text>Cambiar email</Text>
+      </Pressable>
+      <Pressable
+        style={styles.primaryButton}
+        onPress={() => changeProfile('John Doe','johndoe@gmail.com')}
+      >
+        <Text>Resetear nombre</Text>
+      </Pressable>
+
     </View>
   )
 }
--- src/presentation/settings/SettingsScreen.tsx
+++ src/presentation/settings/SettingsScreen.tsx
@@ -1,11 +1,40 @@
-import { Text, View } from "react-native"
+import { Pressable, Text, View } from "react-native"
 import { styles } from "../../config"
+import { useCounterStore } from "../store/counter-store"
+import { useNavigation } from "@react-navigation/native";
+import { useEffect } from "react";
+
 
 
 export const SettingsScreen = () => {
+
+  const count = useCounterStore(state => state.count);
+  const increaseBy = useCounterStore(state => state.incrementBy);
+
+  const navigation = useNavigation();
+
+  useEffect(()=>{
+    navigation.setOptions({
+      title:`Count: ${count}`
+    })
+  },[count]);
+
   return (
-    <View style= {styles.container}>
-      <Text>SettingsScreen</Text>
+    <View style={styles.container}>
+
+      <Text style={styles.title}>Counter: {count}</Text>
+
+      <Pressable
+        onPress={() => increaseBy(+1)}
+        style={styles.primaryButton}>
+        <Text>+1</Text>
+      </Pressable>
+      <Pressable
+        onPress={() => increaseBy(-1)}
+        style={styles.primaryButton}>
+        <Text>-1</Text>
+      </Pressable>
+
     </View>
   )
 }
